38af262a62ab0313de2a6cec9a9dffb72aae695f
lamsyah_bilal <lamsyahbilal1996@gmail.com>
2024-12-09 22:19:23 +0100

    create header component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div >
+      <h1 className='text-8xl text-red-400'>home</h1>
     </div>
   )
 }
